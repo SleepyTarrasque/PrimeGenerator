8689716c4578d38684e15940feb0dc920d8dfe74
SleepyTarrasque <145803476+SleepyTarrasque@users.noreply.github.com>
2023-09-22 14:36:46 -0500

    Add files via upload

--- PrimeGenerator/PrimeGenerator/Program.cs
+++ PrimeGenerator/PrimeGenerator/Program.cs
@@ -11,7 +11,7 @@ namespace PrimeGenerator
     {
         static void Main(string[] args)
         {
-            List<ulong> primes = new List<ulong>(); 
+            List<ulong> susprimes = new List<ulong>(); 
 
             //Fermat's Little Theorem Algorithm
             // 2^(n-1) == 1(mod n)
@@ -20,21 +20,59 @@ namespace PrimeGenerator
             
             ulong testnum = 2;
 
-            while (testnum < 1500)
+            // Generate a list of possible primes
+            while (testnum <= 1024)
             {
                 Console.WriteLine($"\nTesting {testnum}:\nThe result is 2^{testnum-1} = {(Math.Pow(2, testnum-1) % testnum)}(mod{testnum})");
                
                 
                 if ((Math.Pow(2, testnum - 1) % testnum) == 1)
                 {
-                    primes.Add(testnum);
+                    susprimes.Add(testnum);
                     Console.WriteLine($"Suspected prime:    {testnum}");
                 }
                 
                 testnum++;
                 
             }
+
+
+            Console.WriteLine("\n\nPSUEDOPRIME CHECKING:");
+            
+
+            // Run list of suspected primes through sieve
+            for (int n = 0; n < susprimes.Count; n++)
+            {
+                testnum = susprimes[n];
+                for (int i = 0; susprimes[i] < Math.Sqrt(susprimes[n]); i++)
+                {
+                    ulong divisortest = susprimes[i];
+
+                    if (testnum % divisortest == 0)
+                    {
+                        Console.WriteLine($"{testnum} is a psuedoprime divisible by {divisortest}.\n");
+                        susprimes.RemoveAt(i);
+                    }
+                }
+            }
+
+            Console.WriteLine("\n\nFINAL PRIME LIST:");
+            for (int i = 0; i < susprimes.Count; i++)
+            {
+                int m = i % 10;
+                switch (m) 
+                {
+                    case 1: { Console.WriteLine(i + "st prime is:   " + susprimes[i]); break; }
+
+                    case 2: { Console.WriteLine(i + "nd prime is:   " + susprimes[i]); break; }
+
+                    case 3: { Console.WriteLine(i + "rd prime is:   " + susprimes[i]); break; }
+                    
+                    default: { Console.WriteLine(i + "th prime is:   " + susprimes[i]); break; }
+                }
+            }
             Console.ReadLine();
+
         }
     }
 }
